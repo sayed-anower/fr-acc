39b5fa02b927c5978c175c96917042a1e2f8943f
sayed-anower <sayed.anower.17.2@gmail.com>
2026-06-04 23:22:29 +0600

    update config.rs

--- src/config/config.rs
+++ src/config/config.rs
@@ -1,24 +1,20 @@
 use actix_web::web::ServiceConfig;
 use crate::{
     routes::{
+        index_file,
         signup,
         login,
-        forgotten_pwd,
-        verify_fpwd,
-        change_pwd,
-        verify_signup
+        forgotten_password,
+        change_password,
     },
 };
-use crate::routes::index_file;
 
 // App Configuration
 pub fn app_config(cfg: &mut ServiceConfig) {
     cfg
        .service(index_file)
        .service(signup)
-       .service(verify_signup)
        .service(login)
-       .service(forgotten_pwd)
-       .service(verify_fpwd)
-       .service(change_pwd);
+       .service(forgotten_password)
+       .service(change_password);
 }
